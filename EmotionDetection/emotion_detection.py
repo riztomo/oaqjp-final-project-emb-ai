41f70e55f69184d113895e0c951377d35402dbe4
riztomo <rizkyutomo@gmail.com>
2026-05-28 12:10:54 -0400

    Final project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,21 +6,28 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json=input_json, headers=headers)
-    dictionary = json.loads(response.text)
+    
+    if response.status_code == 200:
+        dictionary = json.loads(response.text)
+        
+        emotion = dictionary['emotionPredictions'][0]['emotion']
+        collection = ['anger', 'disgust', 'fear', 'joy', 'sadness']
 
-    emotion = dictionary['emotionPredictions'][0]['emotion']
-    collection = ['anger', 'disgust', 'fear', 'joy', 'sadness']
+        largest_value = 0
+        largest_key = ''
 
-    largest_value = 0
-    largest_key = ''
+        for k in collection:
+            if largest_value < emotion[k]:
+                largest_value = emotion[k]
+                largest_key = k
+            else:
+                continue
 
-    for k in collection:
-        if largest_value < emotion[k]:
-            largest_value = emotion[k]
-            largest_key = k
-        else:
-            continue
+        emotion['dominant_emotion'] = largest_key
 
-    emotion['dominant_emotion'] = largest_key
+        return emotion
 
-    return emotion
+    elif response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+
+    return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
--- server.py
+++ server.py
@@ -1,6 +1,39 @@
-from flask import Flask, jsonify, render_template
-import requests
+'''
+This is a Docstring
+'''
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
+@app.route("/")
+def home():
+    '''
+    Docstring
+    '''
+    return render_template("index.html")
+
 @app.route('/emotionDetector')
+def detect_emotion():
+    '''
+    Docstring
+    '''
+    result = emotion_detector(request.args.get('textToAnalyze'))
+
+    if result['dominant_emotion'] is not None:
+
+        output = f"""
+        For the given statement, the system response is 'anger': {result['anger']},
+        'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}
+         and 'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}.
+        """
+    elif result['dominant_emotion'] is None:
+        output = "Invalid text! Please try again!"
+    else:
+        output = "Unknown error"
+
+    return output
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
